74c7a4a03458181789e5584061dbb6be54f84146
Anver Sadutt <anvergdr@gmail.com>
2022-02-08 08:27:51 +0530

    Fix esbuild referencing html-webpack-plugin internally

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+declare type TOptions = {
+    scriptPrefix: string;
+    stylePrefix: string;
+};
+declare function HtmlWebpackPluginInjectThymeLeafTags(this: any, options?: TOptions | {}): void;
+export default HtmlWebpackPluginInjectThymeLeafTags;
